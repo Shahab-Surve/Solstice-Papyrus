3c733cf8eb93e6aeae6b1a4d2d15da322333d570
Shahab-Surve <shahabsurve7@gmail.com>
2024-10-20 19:20:58 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+
+function Logo() {
+  return (
+    <div><img src={logo} className='w-3/12' /></div>
+  )
+}
+
+export default Logo
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    colors:{
+      jet: '#303036',
+      neoBlack: '#050401',
+      white: '#FFFFFF',
+      gray: '#4B5563',
+      red: '#ff0000',
+      green: '#008000'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
